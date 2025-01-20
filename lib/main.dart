a9ac4fe6b53f21843e4fd01bbe172242da10f68d
RaFiuL-RaZu <rafi102621@gmail.com>
2025-01-20 15:04:52 +0600

    first coomit

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,46 @@
 
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 main(){
-
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget{
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
 
-   return MaterialApp(
-      theme: ThemeData( primarySwatch: Colors.green ),
-       darkTheme: ThemeData(primarySwatch: Colors.amber),
-       debugShowCheckedModeBanner: false,
-       home:HomeActivity()
-
-   );
-  }
-}
-class HomeActivity extends StatelessWidget{
-  const HomeActivity({Key? key}) : super(key: key);
-
-  Mysnakbar(message,context){
-    ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(message))
+    return  MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.green),
+        darkTheme: ThemeData(primarySwatch: Colors.amber),
+        debugShowCheckedModeBanner: false,
+        home: HomeActivity()
     );
   }
 
+}
+
+class HomeActivity extends StatelessWidget{
+  const HomeActivity({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
-
       appBar: AppBar(
-        title: Text("Inventory App"),
-        titleSpacing: 40,
+        title: Text("New Invention App"),
         centerTitle: true,
-        toolbarHeight: 40,
+        titleSpacing: 40,
         toolbarOpacity: 0.9,
+        toolbarHeight: 60,
         elevation: 10,
-        backgroundColor: Colors.indigoAccent,
         actions: [
-          IconButton(onPressed:(){Mysnakbar("I am Comments",context);}, icon: Icon(Icons.comment)),
-          IconButton(onPressed:(){Mysnakbar("I am Settings",context);}, icon: Icon(Icons.settings)),
-          IconButton(onPressed:(){Mysnakbar("I am Email",context);}, icon: Icon(Icons.email)),
-          IconButton(onPressed:(){Mysnakbar("I am Phone",context);}, icon: Icon(Icons.phone)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.search)),
         ],
-
-      ),
-      body: Text("I am Student of Dinajpur Polytechnic Institute"),
-      floatingActionButton: FloatingActionButton(
-        elevation: 10,
-        child: Icon(Icons.add),
-        onPressed: (){
-          Mysnakbar("I am FloatingAction",context);
-        },
-
       ),
 
     );
   }
-
 
 }
