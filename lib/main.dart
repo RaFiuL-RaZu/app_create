49c4de23c88dbed7482ec96abf1169f378e3cb88
RaFiuL-RaZu <rafi102621@gmail.com>
2025-01-22 09:07:23 +0600

    innit commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,51 @@
 
-void main(){
-  String name="Razu";
-  print(name);
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      darkTheme: ThemeData(primarySwatch: Colors.amber),
+      debugShowCheckedModeBanner: false,
+      home: HomeActivity(),
+    );
+  }
+
+}
+
+class HomeActivity extends StatelessWidget{
+  const HomeActivity({Key? key}) : super(key: key);
+
+  Mysnakbar(message,context){
+    ScaffoldMessenger.of(context).showSnackBar(
+        snackBar(content: Text(message))
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Inovation New App"),
+        centerTitle: true,
+        elevation: 10,
+        toolbarOpacity: 0.9,
+        toolbarHeight: 60,
+        actions: [
+          IconButton(onPressed: (){Mysnakbar("Search Option",context);}, icon: Icon(Icons.search)),
+        ],
+      ),
+      
+    );
+  }
+
 }
