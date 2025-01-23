c245c798f33b7ea35709fbe54d21334af1e74df4
RaFiuL-RaZu <rafi102621@gmail.com>
2025-01-23 11:33:58 +0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -29,26 +29,68 @@ class HomeActivity extends StatelessWidget{
         SnackBar(content: Text(message))
     );
   }
+  MyAlertDialog(context){
+    return showDialog(
+        context: context,
+        builder: (BuildContext context){
+         return Expanded(
+             child: AlertDialog(
+               title: Text("Alert !"),
+               content: Text("Do You Want To Delete?"),
+               actions: [
+                 TextButton(onPressed: (){}, child: Text("Yes")),
+                 TextButton(onPressed: (){}, child: Text("No")),
+               ],
+             )
+         );
+        }
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    ButtonStyle buttonStyle=ElevatedButton.styleFrom(
-      padding: EdgeInsets.all(20),
-      primary: Colors.orangeAccent,
-      onPrimary: Colors.white,
 
-    );
 
     return Scaffold(
-      body:Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          TextButton(onPressed:(){Mysnakbar("This is Button",context);}, child: Text("Text Button")),
-          ElevatedButton(onPressed:(){Mysnakbar("This is Button",context);}, child: Text("Elevated Button"),style: buttonStyle,),
-          OutlinedButton(onPressed:(){Mysnakbar("This is Button",context);}, child: Text("OutLine Button")),
+
+      appBar: AppBar(
+        title: Text("WhatsApp"),
+        backgroundColor: Colors.teal,
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.camera_enhance)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.search)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.more_vert)),
+        ],
+
+      ),
+      body: Center(
+        child: ElevatedButton(onPressed:(){MyAlertDialog(context);}, child:Text(" Click Me")),
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.teal,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white,
+        items: [
+          BottomNavigationBarItem(icon:Icon(Icons.home),label: "Chats"),
+          BottomNavigationBarItem(icon:Icon(Icons.update),label: "Updates"),
+          BottomNavigationBarItem(icon:Icon(Icons.call),label: "Calls"),
 
         ],
-      )
+        onTap: (int index){
+          if(index==0){
+            Mysnakbar("Chating Function",context);
+          }
+          if(index==1){
+            Mysnakbar("See Updates",context);
+          }
+          if(index==2){
+            Mysnakbar("Calling me",context);
+          }
+        },
+      ),
+
     );
   }
 
