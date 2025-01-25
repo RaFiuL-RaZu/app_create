3c40ae2704503b8e8403de32fecce9fa087f8e4c
RaFiuL-RaZu <rafi102621@gmail.com>
2025-01-25 21:29:33 +0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,13 @@ class HomeActivity extends StatelessWidget{
       appBar: AppBar(
         centerTitle: true,
         title: Text("Inovation New App"),
+
+      ),
+      floatingActionButton: FloatingActionButton(
+        elevation: 10,
+        child: Icon(Icons.add),
+        onPressed: (){},
+
       ),
     );
   }
