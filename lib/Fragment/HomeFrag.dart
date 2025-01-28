af0f3173dc41e7f58b0de498ad444130c1325a87
RaFiuL-RaZu <rafi102621@gmail.com>
2025-01-28 15:33:13 +0600

    first commit

--- lib/Fragment/HomeFrag.dart
+++ lib/Fragment/HomeFrag.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class HomeFrag extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
 
-    return Container(
-      child: Center(
-        child: Text("HomeFragment"),
+    return Scaffold(
+      body:Center(
+  child: Image.network("https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdXB3azYxNzkxOTM5LXdpa2ltZWRpYS1pbWFnZS1rb3dic3dsMC5qcGc.jpg"),
+
       ),
     );
   }
