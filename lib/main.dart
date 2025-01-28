d832e0a3721f813b6797918545cc3e28dfbd133d
RaFiuL-RaZu <rafi102621@gmail.com>
2025-01-28 08:58:28 +0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -52,6 +52,20 @@ class HomeActivity extends StatelessWidget{
             ),
 
           ),
+          body: TabBarView(
+            children: [
+              Text("1"),
+              Text("2"),
+              Text("3"),
+              Text("4"),
+              Text("5"),
+              Text("6"),
+              Text("7"),
+              Text("8"),
+
+
+            ],
+          ),
         )
     );
   }
