feabf32cc1b029c728b97b8c0c7c05ab06feabb6
RaFiuL-RaZu <rafi102621@gmail.com>
2025-02-02 20:56:00 +0600

    box commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,31 @@
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
-import 'package:flutter/services.dart';
 
-main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      appBar: AppBar(
-        foregroundColor: Colors.black,
-        centerTitle: true,
-        backgroundColor: Colors.white,
-        leading: Icon(
-          Icons.arrow_back,
-          color: Colors.black,
-        ),
-        title: Text(
-          "Flutter",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 25,
-            letterSpacing: 5,
-          ),
-        ),
-        actions: [Icon(Icons.notifications), SizedBox(width: 10)],
-      ),
-      body: Center(
-        child: Container(
-          height: 300,
-          width: 200,
-          decoration: BoxDecoration(
-            gradient:LinearGradient(colors:[
-              Colors.blue,
-              Colors.green,
-              Colors.orangeAccent,
-            ]),
-          ),
-        ),
+main(){
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}) : super(key: key);
 
-      )
-    ),
-  ));
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeActivity(),);
+  }
+
+}
+class HomeActivity extends StatelessWidget{
+  const HomeActivity({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Scaffold();
+  }
+
+
 }
